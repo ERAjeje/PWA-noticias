1006f993868668da29b616da39ea88914388e05a
Edson Ajeje <edson.ajeje@terra.com.br>
2023-04-22 16:14:54 -0300

    refactor: refactored to remove code and create new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,9 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from './Pages/Home'
-import { useEffect, useState } from 'react'
-import Api from './Services/newsService'
-
-type Article = {
-  "title": string,
-  "link": string,
-  "keywords": string | null,
-  "creator": string[],
-  "video_url": string | null,
-  "description": string,
-  "content": string,
-  "pubDate": string,
-  "image_url": string | null,
-  "source_id": string,
-  "category": string[],
-  "country": string[],
-  "language": string,
-}
-
-type AppState = {
-  isLoading: boolean;
-  news: {
-    world: Article[],
-    economy: Article[],
-    technology: Article[]
-  }
-}
+import PostNews from './Pages/Noticia'
 
 function App() {
-  const [state, setState] = useState<AppState>({
-    isLoading: false,
-    news: {
-      world: [],
-      economy: [],
-      technology: []
-    }
-  })
-
-  const handleGetNews = (articles: PromiseSettledResult<any>[]) => {
-    setState({ 
-      news: {
-        world: articles[0].status === "fulfilled" && articles[0].value,
-        economy: articles[1].status === "fulfilled" && articles[1].value,
-        technology: articles[2].status === "fulfilled" && articles[2].value
-      }, 
-      isLoading: false 
-    });
-    console.log('content', state.news.world[0].content)
-  }
-
-  useEffect(() => {
-    setState({ ...state, isLoading: true });
-    Promise.allSettled([
-      Api.getNews('world'),
-      Api.getNews('economy'),
-      Api.getNews('technology')
-    ]).then(articles => handleGetNews(articles))
-  }, []);
 
   return (
     <main>
@@ -66,6 +11,7 @@ function App() {
         <Router>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/:subject/:id' element={<PostNews />} />
           </Routes>
         </Router>
       </section>
---
+++ src/Pages/Noticia/index.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react-refresh/only-export-components */
+import { memo } from "react";
+
+function PostNews() {
+    return (
+        <div>Notícia</div>
+    );
+}
+
+export default memo(PostNews)
